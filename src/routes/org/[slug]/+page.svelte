40eaaae302437311f342f6042ce801ba3c57663d
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-09-11 19:25:03 +0100

    FIX (org/) : fixes paths

--- src/routes/org/[slug]/+page.svelte
+++ src/routes/org/[slug]/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	export let data: App.OrgPage;
+	import { base } from '$app/paths';
 </script>
 
-<a href={'/'}>Go back /</a>
+<a href="{base}/">Go back /</a>
 <h1>{data.org.org}</h1>
 <p>{data.org.cityTown}</p>
 <p>{data.org.postcode}</p>
