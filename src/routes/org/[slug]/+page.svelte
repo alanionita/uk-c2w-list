dbf9e67709fb212d85dc8a7659040f78eb2aa804
Alan Ionita <105461667+alanionita@users.noreply.github.com>
2024-10-09 18:24:56 +0100

    FEAT (page/org): updates the page org to render all the columns for an organisation
    Uses custom Tailwind template

--- src/routes/org/[slug]/+page.svelte
+++ src/routes/org/[slug]/+page.svelte
@@ -3,9 +3,29 @@
 	import { base } from '$app/paths';
 </script>
 
-<a href="{base}/">Go back /</a>
-<h1>{data.org.org}</h1>
-<p>City/Town: {data.org.cityTown}</p>
-<p>Postcode: {data.org.postcode}</p>
-<p>Lat: {data.org.lat}</p>
-<p>Long: {data.org.long}</p>
+<div class="container mx-auto">
+	<a class="text-blue-600" href="{base}/">{`< Back to map`}</a>
+	<div class="px-4 sm:px-0">
+	  <h3 class="text-2xl font-semibold leading-7 text-gray-900">{data.org.Org}</h3>
+	  <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Company details</p>
+	</div>
+	<div class="mt-6 border-t border-gray-100">
+	  <dl class="divide-y divide-gray-100">
+		{#each Object.keys(data.org) as key}
+			{#if data.org[key].length > 0}
+				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+					<dt class="text-sm font-medium leading-6 text-gray-900">{key}</dt>
+					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">{data.org[key]}</dd>
+				</div>
+			{:else}
+				<div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+					<dt class="text-sm font-medium leading-6 text-gray-900">{key}</dt>
+					<dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">N/A</dd>
+				</div>
+			{/if}
+		{:else}
+			<div></div>
+		{/each}
+	  </dl>
+	</div>
+  </div>
